07ef4d5ae80d8cdc48c3cfc22dae934fc0d51f46
thddntjr1234 <thddntjr112233@gmail.com>
2023-10-22 17:57:48 +0900

    test commit

--- src/main/java/com/memopet/memopet/MemopetApplication.java
+++ src/main/java/com/memopet/memopet/MemopetApplication.java
@@ -8,6 +8,8 @@ public class MemopetApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MemopetApplication.class, args);
+
+        System.out.println("ddd");
     }
 
 }
