16421d7ed6ca0033ae2cf5057587bf7c00293f09
jaehoonlee <jaelee9212>
2023-12-17 11:57:09 +0900

    created the following Entity - Address - Member - Meta - Report
    implemented the auditing entity
    - FirstCreatedEntity
    - LastModifiedTimeEntity
    
    add the p6spy dependency in order to check the input values.

---
+++ src/main/java/com/memopet/memopet/Report.java
@@ -0,0 +1,34 @@
+package com.memopet.memopet;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report {
+
+    @Id @GeneratedValue
+    @Column(name="report_id")
+    private Long report_id;
+    @Column(name="report_category")
+    private String reportCategory;
+    @Column(name="report_reason")
+    private String reportReason;
+    @Column(name="reporter_id")
+    private String reporterId;
+    @Column(name="report_target_id")
+    private String reportTargetId;
+    @Column(name="comment_id")
+    private String commentId;
+    @Column(name="pet_species_id")
+    private String petSpeciesId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //private Comment comment;
+}
--- src/test/java/com/memopet/memopet/member/MemberTest.java
+++ src/test/java/com/memopet/memopet/member/MemberTest.java
@@ -1,14 +1,33 @@
 package com.memopet.memopet.member;
 
+import com.memopet.memopet.Member;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.fail;
 
 
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
 public class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
     @Test
-    void test() {
-        //fail("test");
+    void findMember() {
+        Member member = new Member("이재훈");
+
+        em.persist(member);
+
+        assertThat(member.getUsername()).isEqualTo("이재훈");
     }
 
 }
