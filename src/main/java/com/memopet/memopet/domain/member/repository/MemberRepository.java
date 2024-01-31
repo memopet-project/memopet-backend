233c3fd1c07f9896b9f85dccf663239a94612947
seuem <homlesssnail@gmail.com>
2024-01-31 22:26:02 +0900

    prac- petEntity- member findemail

--- src/main/java/com/memopet/memopet/domain/member/repository/MemberRepository.java
+++ src/main/java/com/memopet/memopet/domain/member/repository/MemberRepository.java
@@ -1,10 +1,7 @@
 package com.memopet.memopet.domain.member.repository;
 
 import com.memopet.memopet.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
